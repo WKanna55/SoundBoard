d9046c672574b07c1b0cdc52e0ee463bdeaac149
Wkana <willian.kana@tecsup.edu.pe>
2024-10-09 15:51:54 -0500

    Initial Commit

---
+++ KanaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanaSoundBoard
+//
+//  Created by Willian Kana Choquenaira on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
